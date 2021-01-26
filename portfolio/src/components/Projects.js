5f445f4e0a58f4e74c9fcb3a39492e3b84d4331d
keeks5456 <Akikogreen08@gmail.com>
2021-01-25 23:11:39 -0800

    still need to crop images better, add just breathe content

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,14 +1,16 @@
 import doggyDates from '../projectImage/DoggyDates.png';
 import qpq from '../projectImage/QPQ.gif';
-import voluntheroes from '../projectImage/Voluntheroes.mov';
+// import voluntheroes from '../projectImage/Voluntheroes.mov';
+import Voluntheroes from '../projectImage/Voluntheroes.png'
 function Projects() {
 	console.log(doggyDates);
 	return (
 		<div>
 			<h1>My Personal & Group Projects</h1>
 			<div className="projects-container">
-				<div className="project-title">Voluntheroes</div>
+				<div className="project-title">DoggyDates</div>
 				<img className="project-image" src={doggyDates} alt="doggy-dates-image" />
+                <a className='lnk' href="https://youtu.be/egtJ2lOPFkM" rel="noreferrer" target='_blank'>DEMO</a>
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
 			</div>
@@ -21,28 +23,24 @@ function Projects() {
 			</div>
 
 			<div className="projects-container">
-				<div className="project-title">QPQ</div>
+				<div className="project-title">Voluntheroes</div>
+                <img className="project-image" src={Voluntheroes} alt="qpq-gif" />
                 <a className='lnk' href="https://www.youtube.com/embed/6JEI141ehxA" rel="noreferrer" target='_blank'>DEMO</a>
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
 			</div>
 
 			<div className="projects-container">
-				<div className="project-image">image here</div>
+				<div className="project-title">Just Breathe</div>
+				<img className="project-image" src={qpq} alt="qpq-gif" />
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
 			</div>
 
-			<div className="projects-container">
-				<div className="project-image">image here</div>
-				<div className="project-description">description here</div>
-				<div className="github-link">link here</div>
-			</div>
 		</div>
 	);
 }
 
 export default Projects;
 
-// doggy dates youtube https://youtu.be/egtJ2lOPFkM
-//qpq
+
