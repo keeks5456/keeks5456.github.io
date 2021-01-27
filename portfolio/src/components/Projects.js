a417ff1c2cbee653761523aa5a1e5b0efdc5e96f
keeks5456 <Akikogreen08@gmail.com>
2021-01-27 00:05:39 -0800

    index.html

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,7 +1,10 @@
 import doggyDates from '../projectImage/DoggyDates.png';
 import qpq from '../projectImage/QPQ.gif';
 // import voluntheroes from '../projectImage/Voluntheroes.mov';
-import Voluntheroes from '../projectImage/Voluntheroes.png'
+import Voluntheroes from '../projectImage/Voluntheroes.png';
+
+import { Card } from 'react-bootstrap';
+
 function Projects() {
 	console.log(doggyDates);
 	return (
@@ -10,37 +13,38 @@ function Projects() {
 			<div className="projects-container">
 				<div className="project-title">DoggyDates</div>
 				<img className="project-image" src={doggyDates} alt="doggy-dates-image" />
-                <a className='lnk' href="https://youtu.be/egtJ2lOPFkM" rel="noreferrer" target='_blank'>DEMO</a>
+				<a className="lnk" href="https://youtu.be/egtJ2lOPFkM" rel="noreferrer" target="_blank">
+					DEMO
+				</a>
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
-			</div>
 
-			<div className="projects-container">
-				<div className="project-title">QPQ</div>
-				<img className="project-image" src={qpq} alt="qpq-gif" />
-				<div className="project-description">description here</div>
-				<div className="github-link">link here</div>
-			</div>
+				<Card style={{ width: '25rem' }}>
+					<Card.Img variant="top" src={qpq} alt="qpq-gif" style={{ width: '18rem' }} />
+                    <Card.Body>
+					<Card.Title>QPQ</Card.Title>
+					<Card.Text>
+						Some quick example text to build on the card title and make up the bulk of the card's content.
+					</Card.Text>
+					<a className="github-link" href="https://github.com/bdb2381/qpq-frontend" rel="noreferrer" target="_blank">Link to QPQ Frontend Repository</a>
+                    </Card.Body>
+				</Card>
 
-			<div className="projects-container">
 				<div className="project-title">Voluntheroes</div>
-                <img className="project-image" src={Voluntheroes} alt="qpq-gif" />
-                <a className='lnk' href="https://www.youtube.com/embed/6JEI141ehxA" rel="noreferrer" target='_blank'>DEMO</a>
+				<img className="project-image" src={Voluntheroes} alt="qpq-gif" />
+				<a className="lnk" href="https://www.youtube.com/embed/6JEI141ehxA" rel="noreferrer" target="_blank">
+					DEMO
+				</a>
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
-			</div>
 
-			<div className="projects-container">
 				<div className="project-title">Just Breathe</div>
 				<img className="project-image" src={qpq} alt="qpq-gif" />
 				<div className="project-description">description here</div>
 				<div className="github-link">link here</div>
 			</div>
-
 		</div>
 	);
 }
 
 export default Projects;
-
-
