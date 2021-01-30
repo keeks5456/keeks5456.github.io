f068e5ab05d7d88b206a1689e62d6ed7d50f858a
keeks5456 <Akikogreen08@gmail.com>
2021-01-30 00:26:53 -0800

    cards are responsive yet not in columns

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,4 +1,4 @@
-import doggyDates from '../projectImage/DoggyDates.png';
+import doggyDates from '../projectImage/doggyDates.png';
 import qpq from '../projectImage/QPQ.gif';
 // import voluntheroes from '../projectImage/Voluntheroes.mov';
 import Voluntheroes from '../projectImage/Voluntheroes.png';
@@ -11,37 +11,96 @@ function Projects() {
 		<div>
 			<h1>My Personal & Group Projects</h1>
 			<div className="projects-container">
-				<div className="project-title">DoggyDates</div>
-				<img className="project-image" src={doggyDates} alt="doggy-dates-image" />
-				<a className="lnk" href="https://youtu.be/egtJ2lOPFkM" rel="noreferrer" target="_blank">
+				{/* doggy dates here */}
+				<Card style={{  }}>
+					<Card.Img variant="top" src={doggyDates} alt="doggy dates image" style={{  }} />
+					<hr />
+					<Card.Body>
+						<Card.Title>Doggy Dates</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<a
+							className="github-link"
+							href="https://github.com/carcarvroom/doggy-dates"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Link Doggy Dates Github
+						</a>
+						<br />
+						<a className="lnk" href="https://youtu.be/egtJ2lOPFkM" rel="noreferrer" target="_blank">
+							DEMO
+				</a>
+					</Card.Body>
+				</Card>
+				{/* QPQ */}
+				<Card style={{  }}>
+					<Card.Img variant="top" src={qpq} alt="qpq-gif" style={{  }} />
+					<hr />
+					<Card.Body>
+						<Card.Title>QPQ</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<a
+							className="github-link"
+							href="https://github.com/bdb2381/qpq-frontend"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Link to QPQ Frontend Repository
+						</a>
+					</Card.Body>
+				</Card>
+				{/* Just Breath */}
+				<Card style={{ }}>
+					<Card.Img variant="top" src={qpq} alt="qpq-gif" style={{  }} />
+					<hr />
+					<Card.Body>
+						<Card.Title>Just Breathe</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<a
+							className="github-link"
+							href="https://github.com/bdb2381/qpq-frontend"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Link to QPQ Frontend Repository
+						</a>
+					</Card.Body>
+				</Card>
+				{/* Voluntheroes */}
+
+				<Card style={{ }}>
+					<Card.Img variant="top" src={Voluntheroes} alt="qpq-gif" style={{  }} />
+					<hr />
+					<Card.Body>
+						<Card.Title>Voluntheroes</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the card's
+							content.
+						</Card.Text>
+						<a
+							className="github-link"
+							href="https://github.com/tshlos/voluntheros"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Link to Voluntheroes Github
+						</a>
+						<br />
+						<a className="lnk" href="https://www.youtube.com/embed/6JEI141ehxA" rel="noreferrer" target="_blank">
 					DEMO
 				</a>
-				<div className="project-description">description here</div>
-				<div className="github-link">link here</div>
-
-				<Card style={{ width: '25rem' }}>
-					<Card.Img variant="top" src={qpq} alt="qpq-gif" style={{ width: '18rem' }} />
-                    <Card.Body>
-					<Card.Title>QPQ</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the bulk of the card's content.
-					</Card.Text>
-					<a className="github-link" href="https://github.com/bdb2381/qpq-frontend" rel="noreferrer" target="_blank">Link to QPQ Frontend Repository</a>
-                    </Card.Body>
+					</Card.Body>
 				</Card>
 
-				<div className="project-title">Voluntheroes</div>
-				<img className="project-image" src={Voluntheroes} alt="qpq-gif" />
-				<a className="lnk" href="https://www.youtube.com/embed/6JEI141ehxA" rel="noreferrer" target="_blank">
-					DEMO
-				</a>
-				<div className="project-description">description here</div>
-				<div className="github-link">link here</div>
-
-				<div className="project-title">Just Breathe</div>
-				<img className="project-image" src={qpq} alt="qpq-gif" />
-				<div className="project-description">description here</div>
-				<div className="github-link">link here</div>
 			</div>
 		</div>
 	);
