3e045dedc1c3523a060f0eb65cc1d6fde1630ddb
keeks5456 <Akikogreen08@gmail.com>
2021-03-22 09:00:46 -0700

    better about me

--- portfolio/src/components/QuotesGenerator.js
+++ portfolio/src/components/QuotesGenerator.js
@@ -13,14 +13,14 @@ function QuotesGenerator() {
 	const getQuotes = () => {
 		const inspo = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
 		fetch(inspo).then((res) => res.json()).then((data) => {
-            console.log(data)
+            // console.log(data)
 			let dataQuotes = data.quotes;
 			//gives us our array of quotes
 			let randomNum = Math.floor(Math.random() * dataQuotes.length);
 			//randomization: this is iterating a random number from 0 to length of array, this gives us a random number
 			let randomQuote = data.quotes[randomNum];
 			//accessing a random quote corresponding with that random number
-
+			console.log(randomQuote)
 			setQuote(randomQuote.quote); //now set the new quote
 			setAuthor(randomQuote.author); //set the new author
 		});
